7848eb3d515ce444d30ae2e2634b3225818c8bcd
Vitor Markis <vitormarkis2369@gmail.com>
2023-05-07 04:44:17 -0300

    add working tag to implementations

--- src/components/_AuctionApp/Implementation/index.tsx
+++ src/components/_AuctionApp/Implementation/index.tsx
@@ -1,6 +1,10 @@
+"use client"
 import { CenteredContainer } from "@/components/CenteredContainer"
 import React from "react"
 import { Header } from "./Header"
+import { Grain } from "@styled-icons/material-sharp/Grain"
+import { bounceAnimation } from "./animations"
+import { motion } from "framer-motion"
 
 interface IImplementation extends React.ComponentProps<"div"> {}
 
@@ -12,21 +16,39 @@ export function Implementation({ className, ...rest }: IImplementation) {
       <CenteredContainer className="px-6 mdx:px-12">
         <Header />
         {/* <h3 className="font-black text-2xl tracking-tight font-josefin relative h1-third">Implementações</h3> */}
-        <ul className="text-sm flex flex-wrap gap-x-4 gap-y-2 leading-none">
-          <Item text="Criação de conta" />
-          <Item text="Autenticação via JWT" />
-          <Item text="Autenticação Social &#40;Google e Github&#41;" />
-          <Item text="Criar posts" />
-          <Item text="Ver lances em seu post" />
-          <Item text="Salvar posts" />
-          <Item text="Ver posts salvos" />
-          <Item text="Fazer lances" />
-          <Item text="Ver posts os quais você fez lance" />
-          <Item text="Notificações persistidas em banco de dados" />
-          <Item text="Fazer perguntas no post" />
-          <Item text="Ver suas aquisições" />
-          <Item text="Carrossel de imagens na página do produto" />
-        </ul>
+        <div className="flex flex-col-reverse md:flex-row gap-16 md:gap-6">
+          <div className="grid place-items-center flex-1 grow-[2]">
+            <div
+              className="grid place-items-center rounded-full h-56 w-56 relative"
+              style={{ "--zindex": 15 } as React.CSSProperties}
+            >
+              <motion.div
+                {...bounceAnimation()}
+                className="grid place-items-center rounded-full w-full h-full z-[var(--zindex)]"
+              >
+                <div className="z-[var(--zindex)] inset-[-2px] rounded-full bg-gradient-to-b from-cyan-400 to-cyan-600 absolute" />
+                <div className="z-[var(--zindex)] inset-0 rounded-full bg-gradient-to-b from-cyan-600 to-cyan-500 absolute" />
+                <Grain className="z-[var(--zindex)] text-white h-40 w-40 relative" />
+              </motion.div>
+              <div className="z-[calc(var(--zindex)_-_1)] right-0 left-0 -bottom-4 rounded-[100%] h-6 bg-black absolute blur-xl" />
+            </div>
+          </div>
+          <ul className="flex-1 grow-[3] text-sm flex flex-wrap gap-x-4 gap-y-3 leading-none self-start">
+            <Item text="Criação de conta" />
+            <Item text="Autenticação via JWT" />
+            <Item text="Autenticação Social &#40;Google e Github&#41;" />
+            <Item text="Criar posts" />
+            <Item text="Ver lances em seu post" />
+            <Item text="Salvar posts" working />
+            <Item text="Ver posts salvos" working />
+            <Item text="Fazer lances" />
+            <Item text="Ver posts os quais você fez lance" working />
+            <Item text="Notificações persistidas em banco de dados" working />
+            <Item text="Fazer perguntas no post" working />
+            <Item text="Ver suas aquisições" working />
+            <Item text="Carrossel de imagens na página do produto" />
+          </ul>
+        </div>
       </CenteredContainer>
     </div>
   )
@@ -34,13 +56,22 @@ export function Implementation({ className, ...rest }: IImplementation) {
 
 interface IItem extends React.ComponentProps<"li"> {
   text: string
+  working?: boolean | undefined
 }
 
-export const Item: React.FC<IItem> = ({ text, className, ...rest }) => {
+export const Item: React.FC<IItem> = ({ working, text, className, ...rest }) => {
   const _cn = ` ${className ?? ""}`
 
   return (
-    <li className={"px-3 py-2 rounded-lg border border-neutral-400 border-dashed grow text-center" + _cn} {...rest}>
+    <li
+      className={"relative px-3 py-2 rounded-lg border border-neutral-400 border-dashed grow text-center" + _cn}
+      {...rest}
+    >
+      {working && (
+        <div className="absolute px-1.5 rounded-full py-[3px] leading-none text-[8px] bg-orange-500 text-white -right-2 -top-2">
+          Working
+        </div>
+      )}
       {text}
     </li>
   )
