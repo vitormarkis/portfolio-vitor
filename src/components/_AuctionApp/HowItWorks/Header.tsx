d1330ff9b56acf8748e9c090071ae41c25aff99c
Vitor Markis <vitormarkis2369@gmail.com>
2023-05-07 16:26:32 -0300

    fix how it works title style

--- src/components/_AuctionApp/HowItWorks/Header.tsx
+++ src/components/_AuctionApp/HowItWorks/Header.tsx
@@ -1,6 +1,4 @@
 import React from "react"
-import { motion } from "framer-motion"
-import { animateUnderline } from "../animations"
 
 interface IHeader extends React.ComponentProps<"div"> {}
 
@@ -9,14 +7,10 @@ export function Header({ className, ...rest }: IHeader) {
 
   return (
     <div className={"flex flex-col items-center mb-12" + _cn} {...rest}>
-      <h2 className="font-black mb-0.5 text-4xl text-slate-700 tracking-tight font-josefin relative text-center">
+      <h2 className="font-black mb-0.5 text-4xl tracking-tight font-josefin relative text-center h1-third">
         Como funciona?
-        <motion.i
-          {...animateUnderline(1.5, "7rem")}
-          className="block h-[1px] left-1 bottom-1 w-28 bg-slate-700 absolute"
-        />
       </h2>
-      <p className="text-neutral-400 text-center">
+      <p className="text-slate-700 text-center">
         Criei snippets para explicar a lógica da minha aplicação passo a passo.
       </p>
     </div>
