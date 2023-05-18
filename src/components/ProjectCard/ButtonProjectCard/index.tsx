3797ada6ab1ada3df9427dd6e1a8c03fcc8269ab
Vitor Markis <vitormarkis2369@gmail.com>
2023-05-17 21:49:05 -0300

    change next config

--- src/components/ProjectCard/ButtonProjectCard/index.tsx
+++ src/components/ProjectCard/ButtonProjectCard/index.tsx
@@ -8,30 +8,27 @@ interface IButtonProjectCard extends HTMLAttributes<HTMLButtonElement> {
   target?: React.HTMLAttributeAnchorTarget | undefined
 }
 
-export const ButtonProjectCard = React.forwardRef<
-  HTMLAnchorElement,
-  IButtonProjectCard
->(({ className, url, text, filled, target, ...rest }, ref) => {
-  return (
-    <Link href={url} ref={ref} target={target}>
-      <button
-        className={`outline-accent rounded-full px-8 py-2 text-sm shadow-md inline-block ${
-          className ?? ""
-        } ${
-          filled
-            ? "bg-black text-white border-t-blue-500 border-y border-b-red-500"
-            : "border border-slate-700 text-slate-700"
-        }`}
-        {...rest}
-      >
-        {text}
-      </button>
-    </Link>
-  )
-})
+export const ButtonProjectCard = React.forwardRef<HTMLAnchorElement, IButtonProjectCard>(
+  ({ className, url, text, filled, target, ...rest }, ref) => {
+    return (
+      <Link href={url} ref={ref} target={target}>
+        <button
+          className={`outline-accent rounded-full px-8 py-2 text-sm shadow-md inline-block ${className ?? ""} ${
+            filled
+              ? "bg-black text-white border-t-blue-500 border-y border-b-red-500"
+              : "border border-slate-700 text-slate-700"
+          }`}
+          {...rest}
+        >
+          {text}
+        </button>
+      </Link>
+    )
+  }
+)
 
 type Apple = {
-  banana: "user" | "admin",
+  banana: "user" | "admin"
 }
 
 type Second = Apple["banana"]
