711fe60c0bc42a2065fd76dc20825de9c21f0e74
Vitor Markis <vitormarkis2369@gmail.com>
2023-05-21 03:05:41 -0300

    style toolbar

--- src/components/BlogPageSection/BlogToolbar/index.tsx
+++ src/components/BlogPageSection/BlogToolbar/index.tsx
@@ -15,10 +15,39 @@ export function BlogToolbar({ className, ...rest }: IBlogToolbar) {
   return hasDocument
     ? ReactDOM.createPortal(
         <div
-          className={`flex h-12 bg-red-500 fixed bottom-0 left-0 right-0 z-50 md:hidden`.trim() + _cn}
+          className={
+            `flex justify-around items-center py-3 bg-white shadow-md fixed bottom-3 left-6 right-6 z-20 rounded-xl md:hidden`.trim() +
+            _cn
+          }
           {...rest}
-        ></div>,
+        >
+          <div className="p-2 rounded-lg hover:bg-zinc-200 cursor-pointer">
+            <FunnelIcon />
+          </div>
+          <div className="p-2 rounded-lg hover:bg-zinc-200 cursor-pointer">
+            <FunnelIcon />
+          </div>
+          <div className="p-2 rounded-lg hover:bg-zinc-200 cursor-pointer">
+            <FunnelIcon />
+          </div>
+        </div>,
         document.querySelector("#portal")!
       )
     : null
+}
+
+export function FunnelIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height={24} width={24}>
+      <rect width="256" height="256" fill="none" />
+      <path
+        d="M34.1,61.38A8,8,0,0,1,40,48H216a8,8,0,0,1,5.92,13.38l-67.74,72.31a8,8,0,0,0-2.16,5.47v55.49a8,8,0,0,1-3.56,6.66l-32,21.33A8,8,0,0,1,104,216V139.16a8,8,0,0,0-2.16-5.47Z"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="24"
+      />
+    </svg>
+  )
 }
